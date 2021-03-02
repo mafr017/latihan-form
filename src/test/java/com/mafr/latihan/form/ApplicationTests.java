0bf84cb85b69193bd73f10ac09ee5ffe079f7021
M Aditya Fathur Rahman <madityafr3@gmail.com>
2021-03-02 12:52:52 +0700

    Create Repo

---
+++ src/test/java/com/mafr/latihan/form/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mafr.latihan.form;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
